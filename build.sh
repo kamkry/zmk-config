2d4973b3cebbf2fce9683cd2e35aea7c6e5e2418
Kamil Krysiak <kamkry99@gmail.com>
2025-03-14 14:11:30 +0100

    changes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd ../zmk/app
+cd "$SCRIPT_DIR/../zmk/app"
 
 echo "$SCRIPT_DIR"
 
@@ -28,5 +28,5 @@ wait
 cp "$SCRIPT_DIR/build/left/zephyr/zmk.uf2" "$SCRIPT_DIR/build/chocofi_left.uf2"
 cp "$SCRIPT_DIR/build/right/zephyr/zmk.uf2" "$SCRIPT_DIR/build/chocofi_right.uf2"
 
-cp "$SCRIPT_DIR/build/chocofi_left.uf2" /Volumes/NICENANO/
-cp "$SCRIPT_DIR/build/chocofi_right.uf2" /Volumes/NICENANO\ 1/
+cp "$SCRIPT_DIR/build/chocofi_left.uf2" /Volumes/NICENANO\ 1/
+cp "$SCRIPT_DIR/build/chocofi_right.uf2" /Volumes/NICENANO/
